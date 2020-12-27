caecd7be056113bb910e6dd2f760d4707faa83cb
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2020-12-27 14:53:17 -0600

    Update run.sh

--- MacShellSwift/run.sh
+++ MacShellSwift/run.sh
@@ -2,7 +2,7 @@ echo Enter listening port for your MacC2 server:
 
 read port
 
-sed -e "s/443/$port/g" swiftshell-server.py
+sed -i -e "s/443/$port/g" swiftshell-server.py
 
 docker build -t macshellswift-docker .
 

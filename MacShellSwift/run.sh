9db21b549d15d9c81a86cc74ad8ae35dad7ea4e4
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2020-12-27 14:31:36 -0600

    Create run.sh
    adding a docker setup script

---
+++ MacShellSwift/run.sh
@@ -0,0 +1,11 @@
+echo Enter listening port for your MacC2 server:
+
+read port
+
+sed -i "s/443/$port/g" swiftshell-server.py
+
+docker build -t macshellswift-docker .
+
+docker volume create MacShellSwift
+
+sudo docker run --rm --network="host" -v MacShellSwift:/mss -ti macshellswift-docker
